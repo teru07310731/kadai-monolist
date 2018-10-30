f6c810f06da8e4a87bab5875ae84c742b191ac53
teruteru07310731 <40971412+teru07310731@users.noreply.github.com>
2018-10-30 03:23:08 +0000

    14

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
   has_many :items, through: :ownerships
   has_many :wants, through: :wants
   has_many :want_items, through: :wants, class_name: 'Item', source: :item
-end
+
+  def want(item)
+    self.wants.find_or_create_by(item_id: item_id)
+  end
+
+  def unwant(item)
+    want = self.wants.find_by(item_id: item.id)
+    want.destroy if want
+  end
+
+  def want?(item)
+    self.want_items.include?(item)
+  end
