a659e303ee4875a3f342718b76541f1b029a5a5e
teruteru07310731 <40971412+teru07310731@users.noreply.github.com>
2018-10-30 03:17:36 +0000

    12

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :ownerships
   has_many :users, through: :ownerships
+  has_many :items, through: :ownerships
+  has_many :wants, through: :wants
+  has_many :want_items, through: :wants, class_name: 'Item', source: :item
 end
